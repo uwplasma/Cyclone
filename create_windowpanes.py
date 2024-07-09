6aea2ee96a6775559f5f6bc4a0a6cf03e3c4d225
Dominic Seidita <dseidita1919@gmail.com>
2024-07-09 18:19:11 -0500

    Added new functions for creating multiple independent windowpane shapes.
    create_windowpanes.py:
    Added in function (create_multiple_arbitrary_windowpanes()) which allows users to create windowpane coils with multiple different shapes.
    This function also allows the user to specify the locations of the specific shapes and not place coils in certain locations.
    
    input.LandermanPaul2021_QA:
    Added file with desired plasma boundary to the module for convenience.

--- create_windowpanes.py
+++ create_windowpanes.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import sin, cos, sqrt, atan2
+import jax.numpy as jnp
 
 def maximum_coil_radius(ntoroidalcurves, npoloidalcurves, nfp, stellsym, R0=1.0, R1=0.5):
     '''
@@ -181,8 +182,8 @@ def planar_vector_list(tor_angle, pol_angle):
     (sin(toroidal angle), -cos(toroidal angle), 0)
     (-cos(toroidal angle) * sin(poloidal angle), -sin(toroidal angle) * sin(poloidal angle), cos(poloidal angle))
     '''
-    return np.array([[sin(tor_angle),-cos(tor_angle),0],
-                     [-cos(tor_angle)*sin(pol_angle),-sin(tor_angle)*sin(pol_angle),cos(pol_angle)]])
+    return jnp.array([[jnp.sin(tor_angle),-jnp.cos(tor_angle),0],
+                     [-jnp.cos(tor_angle)*jnp.sin(pol_angle),-jnp.sin(tor_angle)*jnp.sin(pol_angle),jnp.cos(pol_angle)]], float)
 
 def create_arbitrary_windowpanes(ntoroidalcurves, npoloidalcurves, nfp, stellsym, sin_components, cos_components,
                                 rotation_angle=0, winding_surface_function = None, fixed=True, normaltowinding=False,
@@ -278,4 +279,140 @@ def create_arbitrary_windowpanes(ntoroidalcurves, npoloidalcurves, nfp, stellsym
                 curve.fix('yc(0)')
                 curve.fix('zc(0)')
             curves.append(curve)
+    return curves
+
+def create_multiple_arbitrary_windowpanes(ntoroidalcurves, npoloidalcurves, nfp, stellsym, sin_components_0,
+                                          cos_components_0, unique_shapes = 1, shapes_matrix = None,
+                                          rotation_angles=[0], winding_surface_function = None, fixed=True,
+                                          normaltowinding=False,
+                                          order=5, numquadpoints=None, **sin_cos_comps):
+    '''
+    Create ``ntoroidalcurves * npoloidalcurves`` curves of type
+    :obj:`~simsopt.geo.curvexyzfourier.CurveXYZFourier` of order
+    ``order`` that will result in equally angularly spaced windowpane coils
+    lying on a winding surface specified by the input function
+    ''winding_surface_function'' or the default winding surface function
+    ''circular_torus_winding'' after applying
+    :obj:`~simsopt.field.coil.coils_via_symmetries`.
+    The shapes of these coils are specified by ''sin_components_0'' and
+    ''cos_components_0'' as well as any **kwargs, ''sin_components_1'', ''cos_components_1'',
+    ''sin_components_2'', cos_components_2'', etc. which are nested lists or numpy arrays in the
+    format [[planar_vector_1_order_1_component, planar_vector_2_order_1_component],
+    [pv1order2c,pv2order2c],...], where planar vector 1 is the first vector specified
+    by the planar_vector_list function and planar vector 2 is the second
+    vector specified by the planar_vector_list function. The desired shape is
+    then rotated by an angle ''rotation_angles[shape number]''.
+    The number of unique shapes is given by ''unique_shapes''.
+    If ''shapes_matrix'' is specified, it must be a matrix or list with shape
+    ``ntoroidalcurves'' by ''npoloidalcurves`` whose entries are 0,1,..., ''unique_shapes'' - 1.
+    This matrix specifies the locations of the shapes on the winding surface. If -1 is used for an
+    entry, it will result in no coil being initialized in that location.
+    If ''shapes_matrix'' is not specified, the shapes will be laid out as one coil of each shape
+    and then repeat this pattern.
+    If ''fixed=True'' is specified, the center point of the coils will
+    remain fixed in place and will not be dofs.
+    If ''normaltowinding=True'' is specified, the coils will be initialized
+    with their normal vectors pointing along the normal vector of the
+    winding surface instead of towards the primary axis of the stellarator.
+    Additionally, if ''normaltowinding=True'' is specified, the input winding 
+    surface function must also output its normal vector in the form:
+    return xyz_location, normal_vector
+    
+    
+    There is a known bug with normaltowinding=True where the coil shapes
+    are rotated for pi/2 < poloidal angle < 3pi/2.
+
+    Usage example: create 4 rings of 12 base poloidal curves, 
+    which are then rotated 3 times and
+    flipped for stellarator symmetry:
+
+    .. code-block::
+
+        sin_components_0 = [[0.068,0],[0,0.034]]
+        cos_components_0 = [[0,0.034]]
+        sin_components_1 = [[1,0]]
+        cos_components_1 = [[0,0.5]]
+        base_curves = create_arbitrary_windowpanes(4, 12, 3, stellsym=True, sin_components_0, cos_components_0
+                                                   sin_components_1 = sin_components_1, cos_components_1 = cos_components_1)
+        base_currents = [Current(1e5) for c in base_curves]
+        coils = coils_via_symmetries(base_curves, base_currents, 3, stellsym=True)
+    '''
+    shapes_matrix = np.array(shapes_matrix)
+    if shapes_matrix is None:
+        tile = np.array(range(unique_shapes))
+        shapes_matrix = np.resize(tile, (ntoroidalcurves, npoloidalcurves))
+    else:
+        assert shapes_matrix.shape == (ntoroidalcurves, npoloidalcurves)
+    assert len(rotation_angles) == unique_shapes, 'Rotation angles must be a list of length unique_shapes'
+    if numquadpoints is None:
+        numquadpoints = order * 15
+    if winding_surface_function is None:
+        winding_surface_function = circular_torus_winding
+    if not normaltowinding:
+        assert len(winding_surface_function(0,0)) == 3, 'Winding surface not specified as a 3-vector'
+    else:
+        assert len(winding_surface_function(0,0)[0]) == 3, 'Winding surface not specified as a 3-vector'
+    sin_components_all = [None]*unique_shapes
+    cos_components_all = [None]*unique_shapes
+    for i in range(unique_shapes):
+        # Clean up all components
+        if not i == 0:
+            try:
+                sin_components = clean_components(sin_cos_comps['sin_components_{}'.format(i)], order)
+            except:
+                sin_components = clean_components([[0,0]], order)
+            try:
+                cos_components = clean_components(sin_cos_comps['cos_components_{}'.format(i)], order)
+            except:
+                cos_components = clean_components([[0,0]], order)
+        else:
+            sin_components = clean_components(sin_components_0, order)
+            cos_components = clean_components(cos_components_0, order)
+        # Rotate the windowpane coil shapes if there is a rotation angle
+        if not rotation_angles[i]==0:
+            sin_components, cos_components = rotate_windowpane_shapes(sin_components, cos_components, rotation_angles[i], order)
+        sin_components_all[i] = sin_components
+        cos_components_all[i] = cos_components
+    curves=[]
+    from simsopt.geo.curvexyzfourier import CurveXYZFourier
+    for i in range(ntoroidalcurves):
+        tor_angle = (i+0.5)*(2*np.pi)/((1+int(stellsym))*nfp*ntoroidalcurves)
+        for j in range(npoloidalcurves):
+            # Set the shape
+            shape = shapes_matrix[i][j]
+            if not shape == -1:
+                sin_components_this = sin_components_all[shape]
+                cos_components_this = cos_components_all[shape]
+                pol_angle = j*(2*np.pi)/npoloidalcurves
+                curve = CurveXYZFourier(numquadpoints, order)
+                # Initialize the components
+                components = np.array([[0.0]*3 for i in range(1+2*order)])
+                if normaltowinding:
+                    comps, normal_vec = winding_surface_function(tor_angle, pol_angle)
+                    components[0] = list(comps)
+                    tor_winding = atan2(normal_vec[1], normal_vec[0])
+                    pol_winding = atan2(normal_vec[2], sqrt(normal_vec[0] ** 2 + normal_vec[1] ** 2))
+                    '''
+                    # Attempts at solving a known bug
+                    if (np.pi/2) < pol_angle <= (3*np.pi/2):
+                        pol_winding = np.pi - pol_winding
+                        tor_winding = np.pi - tor_winding
+                    '''
+                    planar_vectors = planar_vector_list(tor_winding, pol_winding)
+                else:
+                    components[0] = winding_surface_function(tor_angle, pol_angle)
+                    planar_vectors = planar_vector_list(tor_angle, pol_angle)
+                # Set the sin components
+                for s in range(order):
+                    components[2*s+1] = sin_components_this[s][0]*planar_vectors[0] + sin_components_this[s][1]*planar_vectors[1]
+                # Set the cos components
+                for c in range(order):
+                    components[2*c+2] = cos_components_this[c][0]*planar_vectors[0] + cos_components_this[c][1]*planar_vectors[1]
+                curve.x = list(components.T.flatten())
+                curve.x = curve.x
+                if fixed:
+                    curve.fix('xc(0)')
+                    curve.fix('yc(0)')
+                    curve.fix('zc(0)')
+                curves.append(curve)
     return curves
